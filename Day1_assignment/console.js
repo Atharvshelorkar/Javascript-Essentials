c3abc3b028b60edf1b9cac933ce9a4b925bf7314
Atharvshelorkar <67828921+Atharvshelorkar@users.noreply.github.com>
2020-07-15 18:03:10 +0530

    Add files via upload

---
+++ Day1_assignment/console.js
@@ -0,0 +1,10 @@
+console.log('abc');  
+console.log(1); 
+console.log(true); 
+console.log(null); 
+console.log(undefined);
+
+console.error('This is a simple error'); 
+console.warn('This is a warning.');
+ 
+console.table({'a':1, 'b':2}); 
