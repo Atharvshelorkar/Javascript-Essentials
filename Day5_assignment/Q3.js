86bbf1841e0d4511d32296b13309207f42c1f3af
Atharvshelorkar <67828921+Atharvshelorkar@users.noreply.github.com>
2020-07-21 11:59:02 +0530

    Day5_Assignment

---
+++ Day5_assignment/Q3.js
@@ -0,0 +1,8 @@
+async function fetchJ(){
+    const Response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await Response.json();
+
+    console.log(data);
+}
+
+fetchJ();
