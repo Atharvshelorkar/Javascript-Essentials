f5e675a3d613c0e64dfb69b606da05adf7d2d90d
Atharvshelorkar <67828921+Atharvshelorkar@users.noreply.github.com>
2020-07-23 21:31:13 +0530

    Day3_assignment

---
+++ Day3_assignment/Q1.js
@@ -0,0 +1,13 @@
+num = prompt("Enter the number");
+document.write("the number enter is" ,num);
+
+if(num%2==1){
+    console.log("number is odd");
+    document.write(" and number is odd");
+}
+
+else{
+    console.log("number is even ");
+    document.write(" and number is even");
+}
+
---
+++ Day3_assignment/Q3.js
@@ -0,0 +1,29 @@
+let num = prompt("Enter marks");
+document.write("Marks are" ,num );
+
+ switch(num/10){
+     case 10:
+         case 9:
+            case 8:
+                case 7:
+                    console.log("grade A");
+                    document.write(" and grade is A");
+                    break;
+
+        case 6:
+            case 5:
+                console.log("grade B");
+                document.write(" and grade is B");
+                break;
+
+                case 4:
+                    console.log("grade C");
+                    document.write(" and grade is C");
+                    break;
+
+            default:
+                 console.log("Fail");
+                 document.write(" Fail");
+
+ }
+ 
